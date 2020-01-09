961aa1b53c52429a9b078c1df4f50d6fa8c35137
chestercharles <chestercarmer@icloud.com>
2020-01-09 13:29:45 -0700

    build in start cmd

--- src/client/components/HeaderView.js
+++ src/client/components/HeaderView.js
@@ -1,17 +1,11 @@
 // @flow
 import React from 'react';
 import clsx from 'classnames';
-import {
-  AppBar,
-  Toolbar,
-  Link,
-  Typography,
-  IconButton,
-} from '@material-ui/core';
+import { AppBar, Toolbar, Link, Typography, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../assets/images/transistor-logo.svg';
 
-const style = { 'justifyContent': 'space-between' };
+const style = { justifyContent: 'space-between' };
 const margin = { marginRight: '-20px' };
 type Props = {|
   classes: {|
@@ -28,7 +22,7 @@ type Props = {|
   location: any,
 |};
 
-const HeaderView = ({ classes, open, setOpen, children, location }: Props) =>
+const HeaderView = ({ classes, open, setOpen, children, location }: Props) => (
   <AppBar
     position='static'
     className={clsx(classes.appBar, {
@@ -41,19 +35,24 @@ const HeaderView = ({ classes, open, setOpen, children, location }: Props) =>
       <IconButton
         edge='start'
         className={clsx(classes.menuButton, open && classes.hide)}
-        color='inherit' onClick={() => setOpen(true)}
+        color='inherit'
+        onClick={() => setOpen(true)}
         aria-label='menu'
       >
         <MenuIcon />
       </IconButton>
+      <Typography variant='h6'>Background Geolocation Console</Typography>
       <Typography variant='h6'>
-        Background Geolocation Console
+        <a href='/logs' style={{ color: 'white' }}>
+          (View Logs)
+        </a>
       </Typography>
       <Link edge='end' href='http://transistorsoft.com'>
         <img width='180' style={margin} src={logo} />
       </Link>
     </Toolbar>
     {children}
-  </AppBar>;
+  </AppBar>
+);
 
 export default HeaderView;
